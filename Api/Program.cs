a7342b713efe67a247c97dcd60c56c0befc337bd
mietek442 <96135826+mietek442@users.noreply.github.com>
2025-02-08 12:45:25 +0100

    Add project files.

---
+++ Api/Program.cs
@@ -0,0 +1,45 @@
+
+using Microsoft.OpenApi.Models;
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SupportNonNullableReferenceTypes();
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Api do  Restauracji na obiekotwe", Version = "v1" });
+
+
+               
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
